ecb44cba7e1db26317900a8b31221c51a0ddcdc5
ke-suke <kekeke4060@icloud.com>
2021-08-08 19:52:49 +0900

    middleware, abort(404)

--- resources/views/admin/owners/edit.blade.php
+++ resources/views/admin/owners/edit.blade.php
@@ -30,7 +30,7 @@
                                 <div class="relative">
                                   <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
                                   <input type="email" id="email" name="email" value="{{ $owner->email }}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                {{-- oldは前に入力した値をそのまま初期値に設定するメソッド --}}
+                                {{-- oldは前に入力した値をそのまま初期値に設定するメソッド --}}　
                                 </div>　
                               </div>   
                               <div class="p-2 w-1/2  mx-auto">
--- resources/views/layouts/owner-navigation.blade.php
+++ resources/views/layouts/owner-navigation.blade.php
@@ -5,15 +5,20 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
+                    <div class="w-12">
                     <a href="{{ route('owner.dashboard') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
+                    </div>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('owner/dashboard')" :active="request()->routeIs('owner.dashboard')">
+                    <x-nav-link :href="route('owner.dashboard')" :active="request()->routeIs('owner.dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('owner.shops.index')" :active="request()->routeIs('owner.shops.index')">
+                        店舗情報
                     </x-nav-link>
                 </div>
             </div>
@@ -37,12 +42,12 @@
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('owner.logout') }}">
                             @csrf
-
-                            <x-dropdown-link :href="route('owenr.logout')"
+        
+                            <x-responsive-nav-link :href="route('owner.logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
-                            </x-dropdown-link>
+                            </x-responsive-nav-link>
                         </form>
                     </x-slot>
                 </x-dropdown>
@@ -66,6 +71,9 @@
             <x-responsive-nav-link :href="route('owner.dashboard')" :active="request()->routeIs('owner.dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('owner.shops.index')" :active="request()->routeIs('owner.shops.index')">
+                店舗情報
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
@@ -77,14 +85,14 @@
 
             <div class="mt-3 space-y-1">
                 <!-- Authentication -->
-                <form method="POST" action="{{ route('owenr.logout') }}">
+                <form method="POST" action="{{ route('owner.logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('owner.logout')"
+                    <x-dropdown-link :href="route('owner.logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
-                    </x-responsive-nav-link>
+                    </x-dropdown-link>
                 </form>
             </div>
         </div>
