17783c99b66f3dad4be224b43cdf9d2d21e3278f
ke-suke <kekeke4060@icloud.com>
2021-08-11 22:12:50 +0900

    Image初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 
 
 
@@ -36,6 +37,10 @@ middleware('auth:owners')->group(function(){
     Route::post('updete/{shop}',[ShopController::class, 'update'])->name('shops.update');
 
 });
+
+Route::resource('images', ImageController::class)
+->middleware('auth:owners')->except(['show']);
+
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
