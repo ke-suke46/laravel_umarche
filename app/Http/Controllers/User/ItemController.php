363f36a7ef38ba558487802ec8f52ca5c8216cae
ke-suke <kekeke4060@icloud.com>
2021-08-22 23:13:02 +0900

    show調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+
     public function index()
     {
         $stocks = DB::table('t_stocks')
@@ -38,5 +43,12 @@ class ItemController extends Controller
 
        
         return view('user.index', compact('products'));
+    }
+
+    public function show($id)//ルートパラメーターで$idが渡ってくる
+    {
+        $product = Product::findOrFail($id);
+
+        return view('user.show', compact('product'));
     }
 }
