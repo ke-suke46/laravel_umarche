88999b526820fd5d86de767aba3ef3efb03ceb3d
ke-suke <kekeke4060@icloud.com>
2021-08-29 21:35:53 +0900

    queue

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -33,9 +34,10 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
-        Mail::to('test@example.com')
-        ->send(new TestMail());
 
+        //非同期に送信
+        SendThanksMail::dispatch();
+        
         $categories = PrimaryCategory::with('secondary')
         ->get();
         $products = Product::availableItems()
