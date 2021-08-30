0f4b2b63310e99c0b3b6ad7f469e6e3af412cfd3
ke-suke <kekeke4060@icloud.com>
2021-08-30 23:17:25 +0900

    CartService作成

---
+++ app/Service/CartService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+use App\Models\Product;
+use App\Models\Cart;
+
+class CartService
+{
+ public static function getItemsInCart($items)
+ {
+    $products = []; //空の配列を準備
+    
+   
+    foreach($items as $item)//メールに必要な情報を商品一つずつからとり
+    {
+        $p = Product::findOrFail($item->product_id);
+        $owner = $p->shop->owner->select('name', 'email')->first()->toArray();//オーナー情報を
+        $values = array_values($owner); //連想配列の値を取得
+        $keys = ['ownerName', 'email'];
+        $ownerInfo = array_combine($keys, $values); // オーナー情報のキーを変更
+        
+        $product = Product::where('id', $item->product_id)
+        ->select('id', 'name', 'price')->get()->toArray(); // 商品情報の配列
+    
+        $quantity = Cart::where('product_id', $item->product_id)
+        ->select('quantity')->get()->toArray(); // 在庫数の配列
+
+        $result = array_merge($product[0], $ownerInfo, $quantity[0]); // 配列の結合
+        array_push($products, $result); //配列に追加
+        
+    }
+    dd($products);
+    return $products;//新しい配列に返す
+ }
+}
+
+?>
